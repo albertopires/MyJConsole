5c63cecd4640939342875f2c32e8abb5c90e9793
Alberto Pires <mrpenguin2005@gmail.com>
2016-05-03 18:24:36 -0300

    small stuff

--- src/music/penguin/jtools/JConsole.java
+++ src/music/penguin/jtools/JConsole.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package music.penguin.jtools;
 
 import java.io.FileWriter;
@@ -20,7 +16,7 @@ import javax.management.openmbean.CompositeDataSupport;
 
 /**
  *
- * @author alberto
+ * @author Alberto Pires de Oliveira Neto
  */
 public class JConsole {
 
